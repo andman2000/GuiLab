8c60fc6707756ab5f013efbfb643cb9011e8b50e
Butler <2018abutler@local.tjhsst.edu>
2016-09-21 11:57:38 -0400

    Text View Added

--- app/src/main/java/com/example/a2018abutler/guilab/MainActivity.java
+++ app/src/main/java/com/example/a2018abutler/guilab/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.a2018abutler.guilab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Text View Pressed", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate","TextViewPressed");
             }
         });
     }
